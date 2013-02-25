6632d28537589558e4579b97fa9057089f59b1d6
Daniel Sousa <da.arada@gmail.com>
2013-02-25 14:49:03 +0000

    Users redirects

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    if User.all.empty?
+      redirect_to signup_url
+      flash[:notice] = 'Database without users'
+    elsif !signed_in?
+      redirect_to signin_url
+    end
   end
 
   def help
